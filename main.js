88fe9388944e4ad6d918b0809e63966feee426e8
Kota Matsumoto <gunjo64@icloud.com>
2020-09-22 23:17:47 +0900

    配列とループ処理の組み合わせ

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const score1 = 80;
 const score2 = 90;
 const score3 = 40;
 
-const scores = [80, 90, 40];
+const scores = [80, 90, 40, 70];
 
 // 配列の要素にアクセス ===========================
 // []のなかに何番目の要素か入力
@@ -23,5 +23,18 @@ const scores = [80, 90, 40];
 //結果3
 // console.log(scores.length);
 
+// 配列とループ処理の組み合わせ ===========================
+// //結果 = Score: 80
+// console.log(`Score: ${scores[0]}`);
+// //結果 = Score: 90
+// console.log(`Score: ${scores[1]}`);
+// //結果 = Score: 40
+// console.log(`Score: ${scores[2]}`);
+
+//ループ処理でiが0から3未満(scoresの数だけ)の間1ずつ増やしながら次の処理をする.
+//結果 = 上と同じ
+for (let i = 0; i < scores.length; i++) {
+  console.log(`Score: ${scores[i]}`);
+}
 
 }

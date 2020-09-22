0ecea2af90abd5477421d8e69ffdb0e74335ec51
Kota Matsumoto <gunjo64@icloud.com>
2020-09-22 22:49:20 +0900

    配列の基本生成

---
+++ main.js
@@ -0,0 +1,12 @@
+// コード内に存在する潜在的な問題を早期に発見しやすくなります。
+// strictモードのコードは高速に実行することができる場合がある（JSエンジンによる最適化処理を困難にする誤りを修正するため
+'use strict'
+
+{
+const score1 = 80;
+const score2 = 90;
+const score3 = 40;
+
+const scores = [80, 90, 40];
+console.log(scores);
+}
